aaf29e9ab4b864bd4d712cfdf152e3ac4ae5e4a0
cissoreo <alexishidayat@gmail.com>
2026-06-24 22:07:41 +0700

    update config

--- config.py
+++ config.py
@@ -1,12 +1,16 @@
 import os
 from pathlib import Path
+import sys
 
 from dotenv import load_dotenv
 
 
 
 def load_config():
-    BASE_DIR = Path(__file__).resolve().parent
+    if getattr(sys, "frozen", False):
+        BASE_DIR = Path(sys.executable).parent
+    else:
+        BASE_DIR = Path(__file__).parent
     env_path = BASE_DIR / ".env"
     if not env_path.exists():
         return None
